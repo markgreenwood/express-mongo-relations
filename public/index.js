e6735b65ce9a601a8a44d0b16f89df3373c87621
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-11-07 13:34:12 -0800

    Basic HTML and JS front end is working!

--- public/index.js
+++ public/index.js
@@ -1,7 +1,15 @@
 $.ajax('/api/riders').done(function(data) {
   data.forEach(function(item) {
     var rider = document.createElement('li');
-    rider.text = item.name;
-    $('#riders-list').append(item.name);
+    rider.innerHTML = item.name;
+    $('#riders-list').append(rider);
+  });
+});
+
+$.ajax('/api/teams').done(function(data) {
+  data.forEach(function(item) {
+    var team = document.createElement('li');
+    team.innerHTML = item.team;
+    $('#teams-list').append(team);
   });
 });
